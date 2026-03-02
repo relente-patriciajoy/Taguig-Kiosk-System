2b2ee344f47a85d30bd6257d5155b50c4f996f35
relente-patriciajoy <relente.patriciajoy@gmail.com>
2026-03-02 23:06:13 +0800

    feat: initial angular project setup

---
+++ frontend/src/app/visitor.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VisitorService {
+  // This is the URL of your Python FastAPI backend
+  private apiUrl = 'http://127.0.0.1:8000/scan-id';
+
+  constructor(private http: HttpClient) { }
+
+  // This function will be called when the user clicks "Scan"
+  getScannedData(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
